6cba062c4b929248fb0ba8287e70becc5299a330
pyohei <mukaishohei76@gmail.com>
2017-03-05 01:12:17 +0900

    First init proto type.

---
+++ main.py
@@ -0,0 +1,23 @@
+import re
+
+def replace_to_camel(s):
+    s = s.group(0)
+    if '_' not in s:
+        return s
+    ss = s.split('_')
+    sss = ss[0]
+    for ssss in ss[1:]:
+        sss += ssss[0].upper() + ssss[1:]
+    return sss
+
+def main(s):
+   rrr = re.compile(r'[a-zA-Z_][a-zA-Z_0-9]*', re.MULTILINE | re.DOTALL)
+   return rrr.sub(replace_to_camel, s)
+
+
+if __name__ == '__main__':
+    sample_string = """
+test = "hoge_foo" + "var" + "python_ruby"
+too_foo
+"""
+    print main(sample_string)
