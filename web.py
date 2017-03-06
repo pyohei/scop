cf78e8598d4d5c3341a9221169add3f113f7a220
pyohei <pyohei@users.noreply.github.com>
2017-03-06 09:20:17 +0900

    Fix to use template.

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from bottle import route, run, request
+from bottle import route, run, request, template
 from converter import main as cnv
 
 
@@ -7,7 +7,7 @@ HTML_STRING = """
   <body>
     <form method="POST" action="/w">
       <div>
-        <textarea name="base" cols=80 rows=40></textarea>
+        <textarea name="base" cols=80 rows=40>{{result}}</textarea>
       </div>
       <input type="submit" value="convert">
     </form>
@@ -19,9 +19,10 @@ HTML_STRING = """
 @route('/w', method=['GET', 'POST'])
 def www():
     s = request.forms.get('base', '')
+    c_str = ''
     if s:
         print cnv(s)
-        return cnv(s)
-    return HTML_STRING
+        c_str = cnv(s)
+    return template(HTML_STRING, result=c_str)
 
 run(host='127.0.9.9', port='11111', debug=True, reloader=True)
