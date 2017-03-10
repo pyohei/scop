01c2daf2a40bac3aac42fe6f6d493f90a078e348
pyohei <pyohei@users.noreply.github.com>
2017-03-10 15:52:17 +0900

    fix regular syntax

--- converter.py
+++ converter.py
@@ -19,16 +19,23 @@ def replace_to_space_camel(s):
     if '_' not in s:
         return s
     ss = s.split('_')
-    sss = ss[0][0].upper() + ss[0][1:]
+    print ss
+    if ss[0][0] == '[':
+        sss = '[(' + ss[0][2].upper() + ss[0][3:]
+    else:
+        sss = '[' + ss[0][0].upper() + ss[0][1:]
     for ssss in ss[1:]:
         if not ssss:
             continue
         sss += ' ' + ssss[0].upper() + ssss[1:]
-    return sss
+    if sss[-1] == ']':
+        return sss
+    else:
+        return sss + ']'
 
 
 def main(s):
-    rrr = re.compile(r'[a-zA-Z_][a-zA-Z_0-9]*', re.MULTILINE | re.DOTALL)
+    rrr = re.compile(r'\[?\(?[a-zA-Z_][a-zA-Z_0-9]*', re.MULTILINE | re.DOTALL)
     # return rrr.sub(replace_to_camel, s)
     return rrr.sub(replace_to_space_camel, s)
 
@@ -38,5 +45,6 @@ if __name__ == '__main__':
 test = "hoge_foo" + "var" + "python_ruby"
 too_foo
 tet_
+[(test_hoge_bar)]
 """
     print main(sample_string)
