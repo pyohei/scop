4853da9c577dae23c186eb72ce5ba83628f0a130
pyohei <mukaishohei76@gmail.com>
2017-03-22 23:16:11 +0900

    Fix ui.

--- web.py
+++ web.py
@@ -4,24 +4,40 @@ from converter import main as cnv
 
 HTML_STRING = """
 <html>
+  <head>
+    <style type="text/css">
+    <!--
+      .box {display: inline;}
+    -->
+    </style>
   <body>
-    <form method="POST" action="/w">
+    <h1>Simple String Converter</h1>
+    <form method="POST" action="/">
       <div>
-        <textarea name="base" cols=80 rows=40>{{result}}</textarea>
+        <div class="box">
+            <textarea name="base" cols=50 rows=40>{{base}}</textarea>
+        </div>
+        <div class="box">
+           <input type="submit" value="convert">
+        </div>
+        <div class="box">
+            <textarea name="result" cols=50 rows=40>{{result}}</textarea>
+        </div>
       </div>
-      <input type="submit" value="convert">
     </form>
   </body>
 </html>
 """
 
 
-@route('/w', method=['GET', 'POST'])
+@route('/', method=['GET', 'POST'])
 def www():
     s = request.forms.get('base', '')
     c_str = ''
     if s:
         c_str = cnv(s)
-    return template(HTML_STRING, result=c_str)
+    embeded_texts = {'base': s,
+                     'result': c_str}
+    return template(HTML_STRING, embeded_texts)
 
-run(host='127.0.9.9', port='11111', debug=True, reloader=True)
+run(host='127.0.0.1', port='9999', debug=True, reloader=True)
