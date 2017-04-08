651eeef312541cc29337cb5f1a32df00fa31b4de
pyohei <mukaishohei76@gmail.com>
2017-04-08 14:29:15 +0900

    Create loader

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,2 +1,16 @@
 """Initial for converter operation."""
+import os
+import glob
 
+IGNORE_FILE = ['__init__.py', '_sample.py']
+
+def load_choices():
+    choices = []
+    for f in glob.glob(os.path.dirname(__file__)+'/*.py'):
+        # TODO: Change it into generator.
+        # print(f)
+        choices.append(os.path.basename(f).replace('.py', ''))
+    return choices
+
+
+# print(load_choices())
